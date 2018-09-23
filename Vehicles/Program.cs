ba02f8cdcfde04d903f5de2daf2654dc4a9ff332
Marckenly Estima <timhoward5@yahoo.com>
2018-09-23 11:36:53 -0400

    Add project files.

---
+++ Vehicles/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Vehicles
+{
+    class Program
+    {
+        static void doWork()
+        {
+            Console.WriteLine("Journy by air plane:");
+            Airplane myPlane = new Airplane();
+            myPlane.StartEngine("Contact");
+            myPlane.TakeOff();
+            myPlane.Drive();
+            myPlane.Land();
+            myPlane.StopEngine("Whirr");
+
+            Console.WriteLine("\nJouney by car:");
+            Car myCar = new Car();
+            myCar.StartEngine("Brm brm");
+            myCar.Accelerate();
+            myCar.Drive();
+            myCar.Brake();
+            myCar.StopEngine("Phut phut");
+
+            Console.WriteLine("\nTesting polymorhism");
+            Vehicles v = myCar;
+            v.Drive();
+            v = myPlane;
+            v.Drive();
+        }
+
+        static void Main()
+        {
+            try
+            {
+                doWork();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: {0}", ex.Message);
+            }
+        }
+    }
+}
